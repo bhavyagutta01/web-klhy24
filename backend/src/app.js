ec988db468e4584f87c0018f8db0533d05381927
bhavyagutta01 <2410030045@klh.edu.in>
2026-05-11 11:50:49 +0530

    initial setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,39 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import LoginPage from './Pages/LoginPage/LoginPage';
-import './index.css';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import { HeaderMegaMenu } from './Components/Navbar/HeaderMegaMenu';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan"; // Import morgan
+import connectDB from "./db/dbConnect.js";
+import { config } from "./config.js";
+import authRouter from "./routes/authRouter.js";
+const app = express();
+
+// middlewares
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Be explicit
+  credentials: true, // This is very often the fix for auth headers
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(morgan("dev")); // Add morgan here for request logging
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+connectDB();
 
 
-function App() {
-  return (
-    <Router>
-        <HeaderMegaMenu/>
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<LoginPage/>} />
-            <Route element={<PrivateRoute/>}>
 
-            </Route>
-        </Routes>
-    </Router>
-  )
-}
+app.use("/api/auth", authRouter);
 
-export default App
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("/*name", (req, res) => { res.sendFile(path.join(__dirname, "../../frontend/dist/index.html")) });
+
+app.listen(config.PORT, () => console.log(Server on PORT: ${config.PORT}));
